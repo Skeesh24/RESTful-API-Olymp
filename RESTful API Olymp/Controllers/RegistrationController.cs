0373268b52cfe769a8f62a53b8275e428ad2a8fc
Skeesh24 <d.pacooloff@gmail.com>
2023-02-05 12:47:59 +0500

    Модели для регистрации

---
+++ RESTful API Olymp/Controllers/RegistrationController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RESTful_API_Olymp.Controllers
+{
+    public class RegistrationController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ RESTful API Olymp/ViewModels/RegisterModel.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RESTful_API_Olymp.ViewModels
+{
+    public class RegisterModel
+    {
+        [Required(ErrorMessage = "Email не был указан")]
+        public string? Email { get; set; }
+
+        [Required(ErrorMessage = "Пароль не был указан")]
+        [DataType(DataType.Password)]
+        public string? Password { get; set; }
+
+        [DataType(DataType.Password)]
+        [Compare("Password", ErrorMessage = "Пароль введен неверно")]
+        public string? ConfirmPassword { get; set; }
+    }
+}
