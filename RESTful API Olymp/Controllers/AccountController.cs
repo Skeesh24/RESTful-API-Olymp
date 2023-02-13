d3ada4469eb34906e72f4df6007a477efddf9602
Skeesh24 <d.pacooloff@gmail.com>
2023-02-14 01:15:44 +0500

    аккаунт контроллер

--- RESTful API Olymp/Controllers/AccountController.cs
+++ RESTful API Olymp/Controllers/AccountController.cs
@@ -2,6 +2,8 @@
 using RESTful_API_Olymp.Domain;
 using RESTful_API_Olymp.Domain.Entities;
 using RESTful_API_Olymp.Models;
+using RESTful_API_Olymp.Static_Helper;
+using System.Text.RegularExpressions;
 
 namespace RESTful_API_Olymp.Controllers
 {
@@ -16,53 +18,158 @@ namespace RESTful_API_Olymp.Controllers
 
 
         [HttpGet("account/accounts/search")]
-        public IActionResult Search(string firstName = "", string lastName = "", string email = "", int from = -1, int size = -1)
+        public IActionResult Search(string? firstName, string? lastName, string? email, int from = 0, int size = 10)
         {
-            ViewBag.Title = "Поиск";
-            var vm = new AccountViewModel { Accounts = Db?.Accounts.ToList() };
+            var foundAccount = Db?.Accounts.ToArray();
+            if (firstName != null) 
+                foundAccount = foundAccount?.Where(x => x.FirstName == firstName).ToArray();
 
-            if (firstName != "" && vm != null)
-            {
-                vm.Accounts = vm?.Accounts?.Where(x => x.FirstName == firstName).ToList();
-            }
+            if (lastName != null)
+                foundAccount = foundAccount?.Where(x => x.LastName == lastName).ToArray();
 
-            if (lastName != "" && vm != null)
-            {
-                vm.Accounts = vm?.Accounts?.Where(x => x.LastName == lastName).ToList();
-            }
+            if (email != null)
+                foundAccount = foundAccount?.Where(x => x.Email == email).ToArray();
 
-            if (email != "" && vm != null)
-            {
-                vm.Accounts = vm?.Accounts?.Where(x => x.Email == email).ToList();
-            }
+            foundAccount = 
+                foundAccount
+                .Skip(from)
+                .Take(size)
+                .OrderBy(x => x.Id)
+                .ToArray();
 
-            if (from != -1 && vm != null)
-            {
-                vm.Accounts = vm?.Accounts?.Skip(from).ToList();
-            }
 
-            if (size != -1 && vm != null)
-            {
-                vm.Accounts = vm?.Accounts?.Take(size).ToList();
-            }
-
-            if (vm != null)
-                vm.Accounts = vm?.Accounts?.OrderBy(x => x.Id).ToList();
-
-            return View(vm);
+            Response.StatusCode = 200;
+            return Json(foundAccount);
         }
 
 
 
         [HttpGet/*("accounts/")*/]
-        public IActionResult Accounts(long accountId)
+        public IActionResult Accounts(long? accountId)
         {
-            ViewBag.Title = "Профиль";
-            var vm = new AccountViewModel { Accounts = Db?.Accounts.Where(x => x.Id == accountId).ToList() };
-            return View(vm);
+            if (accountId == null || accountId <= 0)
+                return BadRequest();
+
+            if (!Helper.Authenticate(Request, Db, out int code) && code != 1)
+                return Unauthorized();
+
+            var accountGet = Db?.Types.Where(x => x.Id == accountId).FirstOrDefault();
+            if (accountGet == null)
+                return NotFound();
+
+
+            Response.StatusCode = 200;
+            return Json(accountGet);
         }
 
 
-        
+        [HttpPut]
+        public IActionResult AccountsPut(int? accounId)
+        {
+            if (accounId == null || accounId <= 0)
+                return BadRequest();
+
+            if (!Helper.Authenticate(Request, Db, out int code) && code == 0)
+                return Unauthorized();
+
+            var account = Helper.DeserializeJson<AccountEntity>(Request);
+
+            var putAccount = Db?.Accounts.Where(x => x.Id == accounId).FirstOrDefault();
+            if (putAccount == null)
+            {
+                return NotFound();
+            }
+
+            if (account.FirstName == null || account.FirstName == "" | account.FirstName.Split().Count() == 1)
+                return BadRequest();
+
+            if (account.LastName == null || account.LastName == "" || account.LastName.Split().Count() == 1)
+                return BadRequest();
+
+            if (account.Email == null || account.Email == "" || !Regex.IsMatch(account.Email, "(\\w+([-+.]\\w+)*@\\w+([-.]\\w+)*\\.\\w+([-.]\\w+)*)"))
+                return BadRequest();
+
+            // обновление не своего аккаунта
+            if(account.Id != putAccount.Id)
+                return StatusCode(203);
+
+            // аккаунт с таким имейлом уже существует
+            if (Db.Accounts.Any(x => x.Email == account.Email))
+                return StatusCode(209);
+            
+
+            Db?.Accounts.Remove(putAccount);
+            var newAccount = new AccountEntity
+            {
+                Id = account.Id,
+                Email = account.Email,
+                FirstName = account.FirstName,
+                LastName = account.LastName,
+                Password = account.Password
+            };
+
+
+            Db?.Accounts.Add(newAccount);
+
+
+            Response.StatusCode = 200;
+            Db?.SaveChanges();
+            return Json(newAccount);
+        }
+
+
+
+        [HttpDelete]
+        public IActionResult AccountDelete(int? accountId)
+        {
+            if (accountId == 0 || accountId == null)
+                return BadRequest();
+
+            if (!Helper.Authenticate(Request, Db, out int code) && code == 0)
+                return Unauthorized();
+
+
+            var deleteAccount = Db?.Accounts?.Where(x => x.Id == accountId)?.FirstOrDefault();
+
+            // не ошибка, по тз именно такой статус код 
+            if (deleteAccount == null)
+                return StatusCode(403);
+
+            // удаление не своего аккаунта
+            if (deleteAccount.Id != accountId)
+                return StatusCode(403);
+
+            // аккаунт связан с животным - TODO: уточнить корректность
+            if (Db.Animals.Any(x => x.ChipperId == accountId))
+                return BadRequest();
+
+
+            var newTypeList = Db?.Accounts?.ToList();
+            newTypeList?.Remove(deleteAccount);
+
+
+            // повторная индексация ключей
+            for (var elem = 1; elem <= newTypeList.Count; elem++)
+            {
+                var updateElem = new AccountEntity
+                {
+                    Id = elem,
+                    Email = newTypeList[elem - 1].Email,
+                    FirstName = newTypeList[elem - 1].FirstName,
+                    LastName = newTypeList[elem - 1].LastName,
+                    Password = newTypeList[elem - 1].Password,
+                };
+                newTypeList[elem - 1] = updateElem;
+            }
+
+            Db?.Accounts.RemoveRange(Db?.Accounts);
+            Db?.Accounts.AddRangeAsync(newTypeList);
+
+
+            Db?.SaveChanges();
+            // TODO: уточнить, почему по тз статус код 200, а не 204
+            return Ok();
+        }
+
     }
 }
